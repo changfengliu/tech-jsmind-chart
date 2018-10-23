ad7485bb50695054c9b4c503bdb7c09605c995b2
changfeng.liu <361065934@qq.com>
2018-10-23 10:57:47 +0800

    init cmt

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tech-jsmind-chart
-前端知识体系
+本项目用于整理前端知识体系。
+
+### 预览方法
+clone代码到本地，直接打开根目录下的 index.html。
---
+++ jsmind/util.js
@@ -0,0 +1,10 @@
+var __util = {
+	getQueryString: function(name) {
+		var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+		var r = window.location.search.substr(1).match(reg);
+		if (r != null) {
+			return unescape(r[2]);
+		}
+		return null;
+	}
+}
